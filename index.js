991601db7ba3b68271f56ad2b7ac3ad604a50a73
Mohammad Al Issa <mohammed.alissa627@gmail.com>
2023-09-26 23:40:06 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(inte){
+    while(inte+1>0){
+        console.log(inte);
+        inte-=1;
+    }
+}
+let newmessage= [];
+function writeCards(name,event){
+
+    for(let i=0;i<name.length;i++){
+        newmessage.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+
+    }
+    return newmessage;
+}
